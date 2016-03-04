ef982b6744669a463d242bfbcd05864dbefa2bc3
William of Ockham <certainthings@gmail.com>
2016-03-04 13:01:51 +1100

    receive bacon

--- js/bevel.js
+++ js/bevel.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
   if (window.DeviceOrientationEvent === undefined) {
-    console.log('Device orientation is not supported in this browser: giving up');
+    location.href = 'http://caniuse.com/';
     return;
   }
 
@@ -9,30 +9,23 @@ $(document).ready(function () {
   var $hud = $('.hud');
 
   if (window.location.hash === '#hud') {
-    $('.hud').fadeIn();
+    $hud.fadeIn();
   }
-
-  var opacity = function (yaw) {
-    return (yaw / 180) * 1.5;
-  };
-
-  var status = function (props) {
-    var statuses = [];
-    for (var k in props) {
-      statuses.push(k + ': ' + props[k]);
-    }
-    return statuses.join(', ');
-  };
 
   var orientation = $(window).asEventStream('deviceorientation');
 
-  orientation.onValue(function (e) {
-    $fader.css('opacity', opacity(e.originalEvent.beta));
-    $hud.text(status({
-      opacity: opacity(e.originalEvent.beta),
-      pitch: e.originalEvent.alpha,
-      yaw: e.originalEvent.beta,
-      roll: e.originalEvent.gamma
-    }));
-  })
+  var yaw = orientation.map('.originalEvent.beta');
+  var opacity = yaw.map(function (v) {
+    return (v / 180) * 1.5;
+  });
+  opacity.assign($fader, 'css', 'opacity');
+
+  var status = orientation.map(function (v) {
+    return [
+      'alpha: ' + v.originalEvent.alpha,
+      'beta: ' + v.originalEvent.beta,
+      'gamma: ' + v.originalEvent.gamma
+    ].join('<br>');
+  });
+  status.assign($hud, 'html');
 });
